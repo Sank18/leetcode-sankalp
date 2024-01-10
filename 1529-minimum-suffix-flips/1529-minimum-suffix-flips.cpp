8809fc1459b7dec193baff155c7e1dacbcb10a4e
Sank18 <106996320+Sank18@users.noreply.github.com>
2024-01-10 13:50:05 +0530

    Time: 17 ms (35.56%), Space: 10.7 MB (5.56%) - LeetHub

---
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minFlips(string target) {
+        string news="";
+        
+        news+=target[0];
+        for(int i=1;i<target.size();i++)
+        {
+            if(news[news.size()-1]!=target[i])
+            {
+                news+=target[i];
+            }
+            else
+            {
+                continue;
+            }
+        }
+        //count flips;
+        cout<<news<<endl;
+        if(news.size()==1 && news[0]=='0')
+        {
+            return 0;
+        }
+        if(news[0]=='0')
+        {
+            return news.size()-1;
+        }
+        return news.size();
+        
+    }
+};
