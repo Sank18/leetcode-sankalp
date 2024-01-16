63de393cb7c2327851e32e1a3556b5d62cdadb8f
Sank18 <106996320+Sank18@users.noreply.github.com>
2024-01-17 03:05:31 +0530

    Time: 40 ms (70.80%), Space: 27.7 MB (47.71%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,46 +1,46 @@
 class Solution {
 public:
-    bool f(int i,int j,string &s,string &p,vector<vector<int>>&dp)
+    bool f(int i,int j,string &p,string &s,vector<vector<int>>&dp)
     {
-        if(i==0 && j==0)
+        if(i<0 && j<0)
         {
             return true;
         }
-        if(i>0 && j==0)
+        if(i<0 && j>=0)
         {
             return false;
         }
-        if(i==0 && j>0)
+        if(i>=0 && j<0)
         {
-            for(int k=j;k>=1;k--)
+            for(int k=0;k<=i;k++)
             {
-                if(p[k-1]!='*')
+                if(p[k]!='*')
                 {
                     return false;
                 }
             }
             return true;
         }
-        
         if(dp[i][j]!=-1)
         {
             return dp[i][j];
         }
-        if(s[i-1]==p[j-1] || p[j-1]=='?')
+        if(p[i]==s[j] || p[i]=='?')
         {
-            return dp[i][j]=f(i-1,j-1,s,p,dp);
+            return dp[i][j]=f(i-1,j-1,p,s,dp);
         }
-        else if(p[j-1]=='*')
+        if(p[i]=='*')
         {
-            return dp[i][j]=f(i-1,j,s,p,dp)||f(i,j-1,s,p,dp);
+            return dp[i][j]=f(i-1,j,p,s,dp)||f(i,j-1,p,s,dp);
         }
-        return dp[i][j]=false;
+        return false;
+            
     }
     bool isMatch(string s, string p) {
-        int n=s.size();
-        int m=p.size();
+        int n=p.size();
+        int m=s.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return f(n,m,s,p,dp);
+        return f(n-1,m-1,p,s,dp);
         
     }
 };
