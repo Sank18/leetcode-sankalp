7abbaa8f955c63084d31ab5f7f1a8eee791174f0
Sank18 <106996320+Sank18@users.noreply.github.com>
2024-01-18 00:47:40 +0530

    Time: 61 ms (57.88%), Space: 30.2 MB (18.46%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool solve(int idx, string &s, int minJ, int maxJ, vector<int> &dp){
+        if(idx >= s.length() || s[idx] == '1') return false;
+        if(idx == s.length() - 1) return true;
+        if(dp[idx]!=-1) return dp[idx];
+        dp[idx] = false;
+        for(int i = minJ; i<= maxJ; i++){
+            if(solve(idx + i, s, minJ, maxJ, dp)) return dp[idx] = true;
+        }
+        return dp[idx];
+    }
+    bool canReach(string s, int minJump, int maxJump) {
+        if(s[s.length() - 1] == '1') return false;
+        // applying sliding window in order to save time and reduce time complexity
+       
+        int cnt = 0;
+      
+        for(int i = 0; i<s.length(); i++){
+            if(s[i] == '1'){
+                cnt++;
+                if(cnt >= maxJump) return false;
+            }
+            else{
+                cnt = 0;
+            }
+        }
+        vector<int> dp(s.length(), -1);
+        return solve(0, s, minJump, maxJump, dp);
+    }
+};
