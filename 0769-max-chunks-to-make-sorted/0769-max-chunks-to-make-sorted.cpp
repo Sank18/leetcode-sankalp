33b2507f0d4248b02aecb32d770d2e05304813c1
Sank18 <106996320+Sank18@users.noreply.github.com>
2024-01-24 17:53:03 +0530

    Time: 4 ms (39.88%), Space: 8.5 MB (6.13%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int chunks=1;
+        stack<int>st;
+        vector<int>intervalmax;
+        st.push(arr[0]);
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i]>=st.top())
+            {
+                //i can do partition
+                intervalmax.push_back(st.top());
+                // cout<<"did a partition at "<<" "<<st.top()<<endl;
+                
+                st.push(arr[i]);
+            }
+            else
+            {
+                //i cant do partition ,i have to add to the current part 
+                
+                    //it is not crerating a problem in previous parts
+                    bool flag=false;
+                    //false represnts not creating a problem
+                    int breakingpoint=100000;
+                    for(int j=0;j<intervalmax.size();j++)
+                    {
+                        if(arr[i]<intervalmax[j])
+                        {
+                            breakingpoint=j;
+                            flag=true;
+                            break;
+                        }
+                    }
+                
+                if(flag==false)
+                {
+                    continue;
+                }
+                else
+                {
+                    // cout<<"gave to merge all till partition having max value as"<<intervalmax[breakingpoint]<<endl;
+                    int n=intervalmax.size();
+                    for(int j=n-1;j>=breakingpoint;j--)
+                    {
+                        intervalmax.pop_back();
+                    }
+                    
+                    chunks=intervalmax.size()+1;
+                }
+                
+            }
+        }
+        return intervalmax.size()+1;
+        
+    
+        
+    }
+};
