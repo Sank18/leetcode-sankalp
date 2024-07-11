191ead9260f8aec39438e5d1ac4631a9e8544522
Sank18 <106996320+Sank18@users.noreply.github.com>
2024-07-11 23:09:38 +0530

    Time: 98 ms (49.05%), Space: 13.2 MB (43.98%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,11 +4,11 @@ public:
         int n=nums.size();
         vector<int>dp(n,1);
         int ans=1;
-        for(int i=1;i<nums.size();i++)
+        for(int i=1;i<n;i++)
         {
             for(int j=0;j<i;j++)
             {
-                if(nums[i]>nums[j])
+                if(nums[j]<nums[i])
                 {
                     dp[i]=max(dp[i],1+dp[j]);
                     ans=max(ans,dp[i]);
